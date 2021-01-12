8e2f291df4542b4238c5ac021258ba9e6a574a0a
KushGabani <kushgabz2687@gmail.com>
2021-01-12 17:24:57 +0530

    changes for deployement

--- resp-disease.py
+++ resp-disease.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import librosa
-import tensorflow as tf
 from tensorflow.keras.models import load_model
 
 audioPath = "./uploaded-data/" + os.listdir(os.getcwd() + "/uploaded-data/")[0]
@@ -12,7 +11,6 @@ clip, sr = librosa.load(audioPath)
 mfccs = librosa.feature.mfcc(y = clip, sr = sr, n_mfcc = 40)
 if mfccs.shape[1] == 862:
     model = load_model("./static/tfjs-models/Respiratory-disease-detection-model/respiratory-model.h5")
-    # print(audioPath)
     mfccs = mfccs.reshape(1, 40, 862, 1)
     predictions = model.predict(mfccs)
     predictions =  [(round(pred * 100)) for pred in predictions[0]]
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 let express = require("express");
-let path = require("path")
-let exec = require("child_process").execFile
+let path = require("path");
+let exec = require("child_process").execFile;
 let bodyparser = require("body-parser");
 let multer = require("multer");
 let app = express();
 const port = process.env.PORT || 8888;
 
 let storage = multer.diskStorage({
-  destination: function(req, file, callback) {
+  destination: function (req, file, callback) {
     callback(null, "./uploaded-data");
   },
-  filename: function(req, file, callback) {
+  filename: function (req, file, callback) {
     callback(null, file.originalname);
-  }
+  },
 });
 
-let upload = multer({storage : storage}).single('uploaded-audio');
+let upload = multer({ storage: storage }).single("uploaded-audio");
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
@@ -25,8 +25,8 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get("/", function(req, res) {
-  res.sendFile(path.resolve(__dirname , "static", "Home.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "static", "Home.html"));
 });
 
 app.use(express.static("./static/"));
@@ -69,31 +69,29 @@ app.post("/HeartDisease", function (req, res) {
 //     });
 // });
 
-app.post("/Respiratory/audio", function(req, res) {
+app.post("/Respiratory/audio", function (req, res) {
   console.log("Uploading audio");
-  upload(req, res, function(err) {
-    if(err) {
+  upload(req, res, function (err) {
+    if (err) {
       return res.end("Error uploading file.");
     }
   });
 
-  console.log("File Uploaded Successfully.")
+  console.log("File Uploaded Successfully.");
   console.log("passing it to python");
   var spawn = require("child_process").spawn;
-  var process = spawn("python", [
-    "./resp-disease.py"
-  ]);
+  var process = spawn("python", ["./resp-disease.py"]);
 
   process.stdout.on("data", function (data) {
     console.log("Got output from python");
     res.end(data.toString());
     console.log(data.toString());
-    });
+  });
 });
 
 app.listen(port, function () {
-  exec("VC_redist.x64.exe", function(err, data) {
-    console.log(err);
-  })
+  // exec("VC_redist.x64.exe", function(err, data) {
+  //   console.log(err);
+  // })
   console.log("Serving static on " + port + "....");
 });
